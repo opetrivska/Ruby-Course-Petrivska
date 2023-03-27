6de3f13ba7d0dac682bf6dc7584009b3d25beb1a
opetrivska <oleksandra.petrivska@teladochealth.com>
2023-03-27 15:27:36 +0300

    User model added

--- modules/helpers/helpers.rb
+++ modules/helpers/helpers.rb
@@ -8,4 +8,25 @@ module Helpers
     @login_page.password_field.set password
     @login_page.submit_button.click
   end
+  
+  def register_user(user)
+    @register_page = RegisterPage.new
+    @register_page.load
+
+    @register_page.first_name_field.set user.firstname
+    @register_page.last_name_field.set user.lastname
+    @register_page.username_field.set user.username
+
+    expect(page).to have_selector '.validation-success.field-validation'
+
+    @register_page.email_field.set user.email
+    @register_page.password_field.set user.password
+    @register_page.register_button.click
+
+    @register_page.role_field.select('Software Developer')
+    @register_page.registration_objective_field.select('I want to store my code')
+    @register_page.different_reason_field.set 'Whatever reason' if @register_page.has_different_reason_field?(visible: true)
+    @register_page.get_started_button.click
+  end
+
 end
--- spec/register_spec.rb
+++ spec/register_spec.rb
@@ -1,27 +1,15 @@
 feature 'User registration', js: true do
   scenario 'User can register to the system' do
-    @register_page = RegisterPage.new
-    @register_page.load
+    user = User.new
 
-    user_name = "test#{SecureRandom.uuid}"
-
-    @register_page.signin_button.click
-    @register_page.newfirstname_field.set 'test'
-    @register_page.newlastname_field.set 'user'
-    @register_page.newusername_field.set user_name
-
-    expect(page).to have_selector '.validation-success.field-validation'
-
-    @register_page.newuseremail_field.set "#{user_name}@example.com"
-    @register_page.newuserpassword_field.set 'testuser123'
-    @register_page.confirm_button.click
-    select('Software Developer', from: "Role")
-    select('A different reason', from: "I'm signing up for GitLab because:")
-
-    find("#jobs_to_be_done_other").set 'Whatever reason' if find('#jobs_to_be_done_other', visible: false).visible?
-
-    @register_page.getstarted_button.click
+    register_user user
 
     expect(page).to have_content 'Welcome to GitLab'
+
+    @home_page = HomePage.new
+    @home_page.menu.user_picture.click
+
+    expect(@home_page.menu.user_name).to have_content user.firstname
+    expect(@home_page.menu.user_name).to have_content user.lastname
   end
 end
