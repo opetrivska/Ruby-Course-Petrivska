d8bf3c384f0e11612ad6486335ccba54b6371b84
opetrivska <oleksandra.petrivska@teladochealth.com>
2023-04-10 19:44:59 +0300

    api added

--- modules/models/user.rb
+++ modules/models/user.rb
@@ -7,10 +7,10 @@ class User
   attr_reader :username, :password, :firstname, :lastname, :email
 
   def initialize
-    @username = FFaker::Internet.user_name
-    @password = FFaker::Internet.password
     @firstname = FFaker::Name.first_name
     @lastname = FFaker::Name.last_name
-    @email = FFaker::Internet.safe_email
+    @username = @firstname + @lastname
+    @password = FFaker::Internet.password
+    @email = "#{@username}@example.org"
   end
 end
--- spec/login_spec.rb
+++ spec/login_spec.rb
@@ -2,12 +2,20 @@
 
 feature 'User login', js: true do
   scenario 'User can login to the system' do
-    login_user ENV['USERNAME'], ENV['PASSWORD']
+    new_user = User.new
+
+    response = create_user_api new_user
+    user_id = JSON.parse(response.body)['id']
+
+    login_user new_user.username, new_user.password
     expect(page).to have_content 'Welcome to GitLab'
 
     @home_page = HomePage.new
     @home_page.menu.user_picture.click
 
-    expect(@home_page.menu.user_name).to have_content 'Test User'
+    expect(@home_page.menu.user_name).to have_content new_user.firstname
+    expect(@home_page.menu.user_name).to have_content new_user.lastname
+
+    delete_user_api(user_id)
   end
 end
